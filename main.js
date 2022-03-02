6e88e23445ac46b459e705e5002dc67f1e41a92f
Meke Finau <lonifinau.09@gmail.com>
2022-03-01 19:48:48 -0700

    updates

---
+++ main.js
@@ -0,0 +1,17 @@
+const baseURL = `http://localhost:4000/`
+
+const submitButton = document.querySelector('#submit')
+const searchButton = document.querySelector('#search')
+// const apptButton = document.querySelector('#appointments')
+// const priceButton = document.querySelector('#prices')
+// const styleButton = document.querySelector('#styles')
+
+
+
+
+
+submitButton.addEventListener('click', ) //confirm username and password
+searchButton.addEventListener('click', ) //get data for all appointments on date selected
+// apptButton.addEventListener('click', ) 
+// priceButton.addEventListener('click', ) 
+// styleButton.addEventListener('click', ) 
